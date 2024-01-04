1a0264ede4a39882b33aad9d382d885d16c6da53
Mbele Lebohang <mblleb006@myuct.ac.za>
2024-01-04 19:23:48 +0200

    updated github workflow

--- middleware/app.py
+++ middleware/app.py
@@ -11,10 +11,14 @@ load_dotenv(dotenv_path=Path(__file__).parent.parent.joinpath(".env"))
 
 app = cdk.App()
 
-STAGE=os.getenv('STAGE')
+STAGE=app.node.try_get_context('stage')
 MIDDLEWARE_NAME=os.getenv('MIDDLEWARE_NAME')
 AWS_ACCOUNT_ID=os.getenv('AWS_ACCOUNT_ID')
 AWS_REGION=os.getenv('AWS_REGION')
+
+if not STAGE:
+  raise Exception('CDKContextError: The cdk command is missing the "stage" in the context flag. add -c stage=<stage-name> to your cdk command')
+
 
 if not STAGE or not MIDDLEWARE_NAME or not AWS_ACCOUNT_ID or not AWS_REGION:
   raise Exception('EnvironmentError: The .env file is missing one of the following variables: STAGE, MIDDLEWARE_NAME, AWS_ACCOUNT_ID or AWS_REGION')
